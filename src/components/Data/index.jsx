a825d11c129cfd9713c81890c03e6a56fb7d718d
Hana Spoustová <bernreih@gmail.com>
2023-11-21 13:32:44 +0100

    doplnění otázek

--- src/components/Data/index.jsx
+++ src/components/Data/index.jsx
@@ -1,6 +1,51 @@
 export const dairyData = [
   {
-    question: 'Jaké přídatné látky se mohou přidávat do mléka',
+    question:
+      'Co znamená, když je na krabici mléka napsáno, že je "trvanlivé"?',
+    answer: [
+      'Mléko, které déle vydrží otevřené v lednici.',
+      'Mléko, u kterého bylo dosaženo prodloužení doby trvanlivosti vysokotepelným ošetřením nebo sterilací.',
+      'Mléko, které je svou konzistencí hustší než jinak tepelně zpracovaná mléka a proto déle vydrží',
+      'Mléko, které je možné skladovat po neomezeně dlouhou dobu.',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Jaké je průměrné složení kravského mléka?',
+    answer: [
+      'Mléko obashuje v průměru 4 % tuku, 3,2 % bílkovin, 4,6 % laktózy, zbytek tvoří voda. Obsah tuku a laktózy může být variabilní.',
+      'Mléko obashuje v průměru 4 % tuku, 2 % bílkovin, 3,6 % laktózy, zbytek tvoří voda. Obsah tuku a bílkovin může být variabilní.',
+      'Mléko obashuje v průměru 4 % tuku, 1,7 % bílkovin, 2,6 % laktózy, zbytek tvoří voda. Obsah tuku a bílkovin může být variabilní.',
+      'Mléko obashuje v průměru 4 % tuku, 1,5 % bílkovin, 2,6 % laktózy, zbytek tvoří voda. Obsah tuku a laktózy může být variabilní.',
+    ],
+    correctAnswer: 0,
+  },
+
+  {
+    question: 'Co znamená, když je na lahvi mléka napsáno, že je "čerstvé"?',
+    answer: [
+      'Jako „čerstvé“ mléko lze označit jen mléko, které nebylo nijak tepelně ošetřeno.',
+      'Čerstvé" mléko je mléko, které se do obchodu dostalo do 3 dnů od nadojení.',
+      'Mléko, které má čerstvou chuť a jsou v něm cítit rozkvetlé louky.',
+      'Jako „čerstvé“ mléko lze označit mléko, které nebylo ošetřeno teplotou vyšší než 125 °C.',
+    ],
+    correctAnswer: 3,
+  },
+
+  {
+    question:
+      'Proč se mléko teplně ošetřuje (zahřívá na vysoké teploty) před dalším zpracování?',
+    answer: [
+      'Tepelné ošetření je potřeba pro zachování tekutosti mléka.',
+      'Tepelné ošetření odstraňuje z mléka všechny nežádoucí pachy.',
+      'Teplotní ošetření slouží k odstranění nežádoucích mikroorganismů a prodloužení trvanlivosti mléka, aby se nekazilo.',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'Jaké přídatné látky se mohou přidávat do mléka?',
     answer: [
       'Mohou se přidávat konzervanty pro delší trvanlivost.',
       'Mohou se přidávat konzervatny a barviva pro udržení bílé barvy.',
@@ -46,7 +91,7 @@ export const dairyData = [
     question: 'Jak se vyrábí jogurt?',
     answer: [
       'Kysáním mléka, smetany, podmáslí nebo jejich směsi.',
-      'Kvašením pomocí kvasinek.',
+      'Kvašením mléka pomocí kvasinek.',
       'Přídáním jogurtových zahušťovadel.',
     ],
     correctAnswer: 0,
@@ -90,23 +135,20 @@ export const dairyData = [
     correctAnswer: 3,
   },
   {
-    question: 'Jaký je obecný popis výroby sýra?',
-    answer: [
-      'Tepelné ošetření mléka a jeho zchlazení, přídavek mlékárenských kultur, přídavek syřidla, krájení, dohřívání, formování, solení, popřípadě zrání.',
-      'Tepelné ošetření mléka, přídavek bakterií, přídavek syřidla, krájení, dohřívání, formování, solení a zrání',
-      'Tepelné ošetření mléka a jeho zchlazení, čekání na přirozené srážení mléka',
-    ],
-    correctAnswer: 0,
+    question:
+      'Jaké další druhy mléka, kromě kravského, jsou nejvíce využívány k výrobě mléčných výrobků?',
+    answer: ['Ovčí a bůvolí.', 'Kozí a jačí.', 'Kozí a ovčí'],
+    correctAnswer: 2,
   },
 
   {
     question: 'Co je sýření a syřidlo?',
     answer: [
-      'Syřidlo je enzym, který štěpí mléčnou bílkovinu. Přídavek syřidla způsobuje sýření (srážení), proces, při kterém se z mléka stane hmota konzistence pudinku.',
       'Syřidlo je uměle vytvořená látka, která štěpí mléčný cukr. Přídavek syřidla způsobuje sýření (srážení), proces, při kterém se z tekutého mléka stane hmota konzistence pudinku.',
       'Syřidlo je produkt sýření. Syřidlo dává sýrům nezaměnitelnou chuť a aroma typické pro každý sýr.',
+      'Syřidlo je enzym, který štěpí mléčnou bílkovinu. Přídavek syřidla způsobuje sýření (srážení), proces, při kterém se z mléka stane hmota konzistence pudinku.',
     ],
-    correctAnswer: 0,
+    correctAnswer: 2,
   },
   {
     question: 'Jaký je rozdíl mezi jogurtem a kefírem?',
@@ -183,12 +225,12 @@ export const dairyData = [
   {
     question: 'Který mléčný produkt je dobrým zdrojem bílkovin?',
     answer: [
-      'Tvaroh.',
       'Kondenzované mléko.',
       'Sladká smetana.',
       'Mléčná čokoláda.',
+      'Tvaroh.',
     ],
-    correctAnswer: 0,
+    correctAnswer: 3,
   },
   {
     question: 'Proč je mléko dobré pro zdravý růst kostí?',
@@ -204,7 +246,7 @@ export const dairyData = [
     question: 'Proč by děti měly pít mléko?',
     answer: [
       'Kvůli jeho vysokému obsahu proteinů a vápníku.',
-      'Protože chutná dobře s čokoládou.',
+      'Protože chutná dobře.',
       'Kvůli jeho barvě.',
       'Kvůli vysokému obsahu cukru.',
     ],
@@ -223,12 +265,77 @@ export const dairyData = [
 ];
 
 export const bakeryData = [
-  { question: '', answer: ['', '', '', ''], correctAnswer: '' },
+  {
+    question:
+      'Co to znamená, když je chleba nebo pečivo označeno jako "pšeničné"?',
+    answer: [
+      'Obsahují pouze pšeničnou mouku.',
+      'Obsahují min. 90 % pšeničné mouky ze všech použitých mouk.',
+      'Obsahují min. 85 % pšeničné mouky ze všech použitých mouk.',
+      'Obsahují min. 80 % pšeničné mouky ze všech použitých mouk.',
+    ],
+    correctAnswer: 1,
+  },
+  {
+    question:
+      'Co to znamená, když je chleba nebo pečivo označeno jako "žitné"?',
+    answer: [
+      'Obsahují pouze žitnou mouku.',
+      'Obsahují min. 90 % žitné mouky ze všech použitých mouk.',
+      'Obsahují min. 85 % žitné mouky ze všech použitých mouk.',
+      'Obsahují min. 80 % žitné mouky ze všech použitých mouk.',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question:
+      'Co to znamená, když je chleba nebo pečivo označeno jako "žitnopšeničné"?',
+    answer: [
+      'Obsahují 50 % žitné mouky a 50 % pšeničné mouky.',
+      'Obsahují více než 50 % žitné mouky a více než 20 % pšeničných mouk.',
+      'Obsahují více než 50 % žitné mouky a více než 15 % pšeničných mouk.',
+      'Obsahují více než 50 % žitné mouky a více než 10 % pšeničných mouk.',
+    ],
+    correctAnswer: 3,
+  },
+
+  {
+    question:
+      'Co to znamená, když je chleba nebo pečivo označeno jako "pšeničnožitné"?',
+    answer: [
+      'Obsahují 50 % žitné mouky a 50 % pšeničné mouky.',
+      'Obsahují více než 50 % pšeničné mouky a více než 20 % žitných mouk.',
+      'Obsahují více než 50 % pšeničné mouky a více než 15 % žitných mouk.',
+      'Obsahují více než 50 % pšeničné mouky a více než 10 % žitných mouk.',
+    ],
+    correctAnswer: 3,
+  },
+
+  {
+    question: 'Co je to pekárna?',
+    answer: [
+      'Provozovna, ve které se prodává pečivo.',
+      'Provozovna, ve které proběhly všechny části výroby pekařského výrobku od skladování surovin přes přípravu a zpracování těst po finální tepelnou úpravu.',
+      'Provozovna, do které se dovážejí suroviny pro výrobu pekařských výrobků a ve které probíhá jejich výroba',
+      'Provozovna, ve které proběhly všechny technologické operace potřebné pro výrobu pekařského výrobku od skladování surovin přes přípravu a zpracování těst. Dopákání výrobků probíhá až na ve specializovaných prodejnách.',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Jaké jsou hlavní suroviny pro výrobu pekárenských výrobků?',
+    answer: [
+      'Mouka, voda, sůl, droždí.',
+      'Mouka, voda, sůl, tuk, droždí.',
+      'Mouka, voda, sůl, kypřící prášek',
+      'Mouka, voda, sůl, máslo, droždí.',
+    ],
+    correctAnswer: 0,
+  },
 ];
 
 export const meatData = [
-  { question: '', answer: ['', '', '', ''], correctAnswer: '' },
-
   {
     question: 'Co je to maso v tom nejširším slova smyslu?',
     answer: [
@@ -245,7 +352,7 @@ export const meatData = [
       'Maso od kostí odděluje robot, který sekáčkem odděluje maso od kostí. Používá se do salámů.',
       'Maso od kostí odděluje mechanicky řezník sekáčkem v řeznictví. Používá se zejména do salámů a do drůbežích mělněných výrobků.',
       'Maso se od kostí mechanicky odděluje na jatkách. Používá se zejména do salámu a do drůbežích mělněných výrobků.',
-      'Maso a kosti se rozdrtí a masová pasta je pak vytlašována přes speciálně řešená síta, a tak oddělena od kostí.Používá se zejména do salámu a do drůbežích mělněných výrobků.',
+      'Maso a kosti se rozdrtí a masová pasta je pak vytlašována přes speciálně řešená síta, a tak oddělena od kostí. Používá se zejména do salámu a do drůbežích mělněných výrobků.',
     ],
     correctAnswer: 3,
   },
@@ -282,7 +389,7 @@ export const meatData = [
     question:
       'Co to jsou fermentované trvanlivé salámy a jaké výrobky do nich patří?',
     answer: [
-      'Tento druh salámů se nyní již nevyrábí.',
+      'Tento druh salámů se už nevyrábí.',
       'Salámy vyrobené fermentací masa pomocí bakterií, které přeměňují cukry na kyselinu mléčnou (dojde k okyselení) díky čemuž si zachovávají svou trvanlivost. Patří sem poličan, uherský salám, lovecký salám nebo herkules',
       'Jsou to salámy, které prošly fází namáční v alkoholu. Patří sem poličan, uherský salám, lovecký salám nebo herkules',
     ],
@@ -290,14 +397,14 @@ export const meatData = [
   },
 
   {
-    question: 'Kde se porážejí zvířata?',
+    question: 'Kde se převážně porážejí zvířata?',
     answer: [
-      'Na jatkách',
       'Kdekoliv.',
-      'U řezníka v řeznictví',
+      'U řezníka v řeznictví.',
       'U majitele zvířete na zahradě.',
+      'Na jatkách.',
     ],
-    correctAnswer: 0,
+    correctAnswer: 3,
   },
 
   {
@@ -328,7 +435,7 @@ export const meatData = [
 
   {
     question:
-      'Jak se nazývá proces při výrobě masných výrobků, při kterém jsou výrobky plněny do vhodných technologických obalů, které jim dodávají tvar a velikost?',
+      'Jak se nazývá proces při výrobě masných výrobků, při kterém jsou výrobky plněny do vhodných technologických obalů (např. střívek), které jim dodávají tvar a velikost?',
     answer: [
       'Balení',
       'Narážení a tvarování.',
@@ -339,11 +446,11 @@ export const meatData = [
   },
 
   {
-    question: 'Co je to jitrnice?',
+    question: 'Z čeho se vyrábí jitrnice?',
     answer: [
-      'Masný výrobek, který se vyrábí zejména z tučného vepřového masa, máčených housek, koření a vývaru.',
-      'Masný výrobek, který se vyrábí zejména z jater, tučného vepřového masa, plic, máčených housek, koření a vývaru.',
-      'Masný výrobek, který se vyrábí zejména z tučného vepřového masa, máčených housek, koření a vývaru.',
+      'Z tučného vepřového masa, máčených housek, koření a vývaru.',
+      'Zejména z jater, tučného vepřového masa, plic, máčených housek, koření a vývaru.',
+      'Zejména z tučného vepřového masa, máčených housek, koření a vývaru.',
     ],
     correctAnswer: 1,
   },
@@ -376,8 +483,232 @@ export const meatData = [
     ],
     correctAnswer: 3,
   },
+
+  {
+    question: 'K čemu se používá v potravinářství krev poražených zvířat?',
+    answer: [
+      'Krev se pro potravinářské účely nevyužívá.',
+      'Krev se využívá pouze pro technické účely.',
+      'Jako surovina pro výrobu některých masných výrobků, jako jsou jelita.',
+      'Krev se využívá pouze pro krmné účely.',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'K čemu se používá v potravinářství krev poražených zvířat?',
+    answer: [
+      'Krev se pro potravinářské účely nevyužívá.',
+      'Krev se využívá pouze pro technické účely.',
+      'Mimo jiné jako surovina pro výrobu některých masných výrobků, např. jelita.',
+      'Krev se využívá pouze pro krmné účely.',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'Který masný výrobek je typický pro Itálii?',
+    answer: [
+      'Italské párečky.',
+      'Římský salám.',
+      'Milánské klobásy.',
+      'Parmská šunka.',
+    ],
+    correctAnswer: 3,
+  },
+
+  {
+    question:
+      'Po kterém českém městě nese název jedna z našich nejslavnějších šunek?',
+    answer: ['Brno.', 'Olomouc.', 'Ostrava.', 'Praha.'],
+    correctAnswer: 3,
+  },
 ];
 
 export const fruitVegData = [
-  { question: '', answer: ['', '', '', ''], correctAnswer: '' },
+  {
+    question: 'Co je to kompot?',
+    answer: [
+      'Celé nebo krájené ovoce, které bylo vyhozeno na kompost.',
+      'Celé nebo krájené ovoce, které se obvykle zalévá slaným kořeněným nálevem a následně prošlo sterilací.',
+      'Celé nebo krájené ovoce, obyvkle zalité sladkým cukerným roztokem, které bylo sterilováno.',
+      'Celá nebo krájená zelenina, která prošla sterilácí a následně byla zalita cukerným roztokem.',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'Jaký je rozdíl mezi džemem a marmeládou?',
+    answer: [
+      'Žádný.',
+      'Marmelády jsou vyrobeny z citrusového ovoce, džemy jsou vyrobeny z ostatních druhů ovoce.',
+      'Džemy jsou jen označení pro dovezené marmelády z jiné země.',
+      'Marmelády jsou vyrobeny z jakéhokoliv ovoce, kromě citrusů.',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Jaký je rozdíl mezi džemem a marmeládou?',
+    answer: [
+      'Žádný.',
+      'Marmelády jsou vyrobeny z citrusového ovoce, džemy jsou vyrobeny z ostatních druhů ovoce.',
+      'Džemy jsou jen označení pro dovezené marmelády z jiné země.',
+      'Marmelády jsou vyrobeny z jakéhokoliv ovoce, kromě citrusů.',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Jaké technologie se používají k pěstování zeleniny?',
+    answer: ['Skleníky.', 'Hydroponie.', 'Tradiční pole.', 'Všechny možnosti.'],
+    correctAnswer: 3,
+  },
+
+  {
+    question: "Na kterých výrobcích najdeme označení 'zpracovaná zelenina'?",
+    answer: [
+      'Na výrobcích, které výhradně obsahují nakrájenou zeleninu na kostičky.',
+      'Na výrobcích, které obsahují převážně zeleninu a které se upravují konzervováním.',
+      'Na výrobcích zpracovaných v rychlém občerstvení.',
+      'Na výrobcích, které obsahují zpracovanou zeleninu výhradně z biologického zemědělství.',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Co je to fermentace zeleniny?',
+    answer: [
+      'Proces, při kterém se zelenina povaří ve vodě',
+      'Proces, při kterém se zelenina smíchá se sladidlem',
+      'Proces, při kterém se zelenina nakládá a kvasí',
+      'Proces, při kterém se zelenina promíchá s různými kořením',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'Co znamená písmeno A na balení brambor?',
+    answer: [
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor je pevná, nerozvařují se a jsou lojovité, čímž se hodí do salátů nebo jako příloha.',
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor je měkká a hodí se na výrobu kaší a těst.',
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor polopevná, slabě rozvařují a hodí se pro přípravu jídel všeho druhu a jako příloha',
+    ],
+    correctAnswer: 0,
+  },
+
+  {
+    question: 'Co znamená písmeno B na balení brambor?',
+    answer: [
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor je měkká a hodí se na výrobu kaší a těst.',
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor polopevná, slabě se rozvařují a hodí se pro přípravu jídel všeho druhu a jako příloha',
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor je pevná, nerozvařují se a jsou lojovité, čímž se hodí do salátů nebo jako příloha.',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Co znamená písmeno C na balení brambor?',
+    answer: [
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor je pevná, nerozvařují se a jsou lojovité, čímž se hodí do salátů nebo jako příloha.',
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor je měkká a hodí se na výrobu kaší a těst.',
+      'Označuje tzv. varný typ, a znamená to, že konzistence brambor polopevná, slabě rozvařují a hodí se pro přípravu jídel všeho druhu a jako příloha',
+    ],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Jaký je princip výroby kandovaného ovoce?',
+    answer: [
+      'Ovoce se míchá s cukrem a poté se z cukru vyndává velkými sýty.',
+      'Do ovoce se vstřikuje cukr pomocí specializovaných stříkaček.',
+      'Povařené ovoce je obaleno v několika vrstvách cukru',
+      'Povařené ovoce nechá několik dní máčet v cukernatém roztoku.',
+    ],
+    correctAnswer: 3,
+  },
+
+  {
+    question: 'Jaké jsou hlavní suroviny v kečupu?',
+    answer: [
+      'Rajčatový protlak, voda a koření ',
+      'Rajčatový protlak, voda, med, balsamikový ocet, popřípadě koření.',
+      'Rajčatový protlak, voda, cukr, kvasný ocet, jedlá sůl, popřípadě koření.',
+      'Rajčatový protlak, voda, cukr, kyselina citronová, sůl, popřípadě koření',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'Které ovoce není vhodné pro skladování v lednici?',
+    answer: ['Jablka.', 'Banány.', 'Hrušky.', 'Jahody.'],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Které ovoce nepatří mezi citrusy?',
+    answer: ['Citrón.', 'Pomeranč.', 'Pomelo.', 'Avokádo.'],
+    correctAnswer: 3,
+  },
+
+  {
+    question: 'Z jakoho ovoce se nevyrábí povidla?',
+    answer: ['Z meruněk.', 'Z broskví.', 'Z jablek.', 'Ze švestek.'],
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'Co jsou to rozinky?',
+    answer: [
+      'Sušené plody borůvek.',
+      'Sušené plody malin.',
+      'Kandované plody borůvek.',
+      'Sušené bobule hroznů révy vinnné.',
+    ],
+    correctAnswer: 3,
+  },
+
+  {
+    question:
+      'Jaký údaj musí být povinně uveden při prodeji všech druhů ovoce a zeleniny?',
+    answer: [
+      'Jakostní třída.',
+      'Odrůda.',
+      'Země původu.',
+      'Botanický název v latině.',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question:
+      'Které ovoce se v našich podmínkách nejvíce využívá pro výrobu ovocných šťáv?',
+    answer: ['Hruška.', 'Jablko.', 'Meruňka.', 'Hrozny révy vinné.'],
+    correctAnswer: 0,
+  },
+
+  {
+    question:
+      'Která technologie pěstování zeleniny je založena na vodním prostředí bez půdy?',
+    answer: ['Tradiční pole.', 'Skleníky.', 'Hydroponie.', 'Kbelíková metoda.'],
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'Jak se vyrábí sušená zelenina?',
+    answer: [
+      'Je sušena na slunci.',
+      'Je sušena v peci.',
+      'Je sušena v sušárnách.',
+      'Všechny možnosti.',
+    ],
+    correctAnswer: 2,
+  },
+
+  {
+    question:
+      'Která z uvedených potravin pomáhá proti zhnědnutí ovoce a zeleniny při sušení?',
+    answer: ['Sůl.', 'Cukr.', 'Bělidlo.', 'Kyselina citronová.'],
+    correctAnswer: 3,
+  },
 ];
