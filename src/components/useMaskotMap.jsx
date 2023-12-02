337ae7e2e06ee2cc2eb688158a714d73dd3f14c4
Your Name <alzbetakacirkova@Alzbeta-MacBook-Air.local>
2023-12-02 18:43:43 +0100

    upraveny maskot na mapě

--- src/components/useMaskotMap.jsx
+++ src/components/useMaskotMap.jsx
@@ -1,11 +1,26 @@
 import { useState, useEffect, useRef } from 'react';
 import MaskotEggHappy from './Modal/img/maskot-egg-happy.png';
 import MaskotTeenHappy from './Modal/img/maskot-teen-happy.png';
+import MaskotAdultHappy from './Modal/img/maskot-adult-happy.png';
+
+const textyOtazek = [
+  'Hladovím...',
+  'Mňam mňam..',
+  'Ty jsi šikula!',
+  'Zase plné bříško!',
+  'Ještě přidat!',
+  'Hmm.. dobrotka...',
+  'Salám by nebyl?',
+  'Dej mi ještě!',
+  'Výborné!'
+];
+
 
 export const useMaskotMap = ({ delay, textProp, currentButtonId }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [isTextVisible, setIsTextVisible] = useState(false);
-  const [maskotImage, setMaskotImage] = useState(MaskotEggHappy); // Nastavte výchozí obrázek maskota na miminko
+  const [maskotImage, setMaskotImage] = useState(MaskotEggHappy); 
+  const [maskotText, setMaskotText] = useState(textyOtazek[0]); // Nastavení výchozího textu
   
   const maskotRef = useRef(null);
   const textRef = useRef(null);
@@ -27,19 +42,26 @@ export const useMaskotMap = ({ delay, textProp, currentButtonId }) => {
   }, [delay]);
 
   useEffect(() => {
-    if (currentButtonId >= 3) { // Změna obrázku maskota od ID 3
-      setMaskotImage(MaskotTeenHappy); // Nastavte obrázek maskota na teen
+    if (currentButtonId >= 6) {
+      setMaskotImage(MaskotAdultHappy); 
+    } else if (currentButtonId >= 3) {
+      setMaskotImage(MaskotTeenHappy);
     } else {
-      setMaskotImage(MaskotEggHappy); // Nastavte obrázek maskota zpět na miminko
+      setMaskotImage(MaskotEggHappy); 
     }
-  }, [currentButtonId]);
+
+    // Tady aktualizujeme text v bublině
+    if (currentButtonId >= 0 && currentButtonId < textyOtazek.length) {
+      setMaskotText(textyOtazek[currentButtonId]);
+    }
+  }, [currentButtonId]); // Přidali jsme závislost na currentButtonId pro aktualizaci textu
 
   return { 
     isVisible, 
     maskotRef, 
     isTextVisible, 
     textRef, 
-    text: textProp,
-    maskotImage, // Přidávám maskotImage do vraceného objektu
+    text: maskotText, // Zde vracíme aktualizovaný text
+    maskotImage, 
   };
 };
