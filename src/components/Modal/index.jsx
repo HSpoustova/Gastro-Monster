35dab1fffde3d751e8a1755d9c430620d5651114
Your Name <alzbetakacirkova@Alzbeta-MacBook-Air.local>
2023-12-02 09:56:58 +0100

    mapa

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,5 +1,6 @@
 import './style.css';
 import jidlo from './img/food.png';
+import { useMaskot } from '../useMaskot';
 
 export const Modal = ({
   toggleModal,
@@ -16,6 +17,12 @@ export const Modal = ({
       newQuestion(false);
     }
   };
+
+  const { isVisible, isTextVisible, text, maskotRef, textRef, isTextShaking } = useMaskot({
+    delay: 400,
+    textProp: 'Mám hlad!',
+  });
+
 
   return (
     <div>
@@ -42,6 +49,24 @@ export const Modal = ({
               </div>
             ))}
           </div>
+          <div className="maskot-container">
+          {isTextVisible && (
+            <div
+              ref={textRef}
+              className={`maskot-text-bubble ${
+                isTextShaking ? 'shaking-text' : ''
+              }`}
+            >
+              {text}
+            </div>
+          )}
+          <div
+            ref={maskotRef}
+            className={`maskot ${isVisible ? 'active' : ''}`}
+          >
+            <img className="maskot-pic" src={maskotAdultNeutral} alt="Maskot" />
+          </div>
+        </div>
           <footer>
             <div className='ok-button'onClick={closeModal}>OK</div>
           </footer>
