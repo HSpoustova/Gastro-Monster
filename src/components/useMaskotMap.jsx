0c032b678f5fbd8287b077c423fdb1962783d629
Your Name <alzbetakacirkova@Alzbeta-MacBook-Air.local>
2023-12-03 12:33:14 +0100

    maskot na mapě upraven

--- src/components/useMaskotMap.jsx
+++ src/components/useMaskotMap.jsx
@@ -2,6 +2,12 @@ import { useState, useEffect, useRef } from 'react';
 import MaskotEggHappy from './Modal/img/maskot-egg-happy.png';
 import MaskotTeenHappy from './Modal/img/maskot-teen-happy.png';
 import MaskotAdultHappy from './Modal/img/maskot-adult-happy.png';
+import MaskotBabyHappy from './Modal/img/maskot-baby-happy.png';
+import MaskotEggSad from './Modal/img/maskot-egg-sad.png';
+import MaskotTeenSad from './Modal/img/maskot-teen-sad.png';
+import MaskotAdultSad from './Modal/img/maskot-adult-sad.png';
+import MaskotBabySad from './Modal/img/maskot-baby-sad.png';
+import MaskotEggNeutral from './Modal/img/maskot-egg-neutral.png';
 
 const textyOtazek = [
   'Hladovím...',
@@ -16,13 +22,18 @@ const textyOtazek = [
   'Juchůů!'
 ];
 const textySpatnychOdpovedi = [
- 'To je špatné..',
+ 'To je špatně..',
  'Jsem chudinka!',
  'Tak dlouho bez jídla.',
  'Ach jo..',
  'Neee!',
  'Makovče!',
- 'Chci jíst!'
+ 'Chci jíst!',
+ 'Břísko kručí!',
+ 'Oprav se.',
+ 'Umíram hlady!',
+ 'Bude někdy jídlo?',
+ 'Troubo',
 ];
 
 export const useMaskotMap = ({ delay, currentButtonId, isCorrectAnswer }) => {
@@ -35,6 +46,7 @@ export const useMaskotMap = ({ delay, currentButtonId, isCorrectAnswer }) => {
   const textRef = useRef(null);
 
   useEffect(() => {
+
     const timerMaskot = setTimeout(() => {
       setIsVisible(true);
     }, delay);
@@ -51,29 +63,45 @@ export const useMaskotMap = ({ delay, currentButtonId, isCorrectAnswer }) => {
   }, [delay]);
 
   useEffect(() => {
-    if (currentButtonId >= 6) {
-      setMaskotImage(MaskotAdultHappy); 
-    } else if (currentButtonId >= 3) {
-      setMaskotImage(MaskotTeenHappy);
-    } else {
-      setMaskotImage(MaskotEggHappy); 
-    }
-
-    
     if (isCorrectAnswer === null) {
-      // Pokud ještě nebyla provedena žádná odpověď, necháme text nezměněný
+      setMaskotImage(MaskotEggNeutral); 
       return;
     }
+  
+    if (isCorrectAnswer) {
+      // Obrázky pro správné odpovědi
+      if (currentButtonId >= 7) {
+        setMaskotImage(MaskotAdultHappy);
+      } else if (currentButtonId >= 4) {
+        setMaskotImage(MaskotTeenHappy);
+      } else if (currentButtonId >= 2) {
+        setMaskotImage(MaskotBabyHappy);
+      } else {
+        setMaskotImage(MaskotEggHappy);
+      }
+    } else {
+      // Obrázky pro špatné odpovědi
+      if (currentButtonId >= 7) {
+        setMaskotImage(MaskotAdultSad);
+      } else if (currentButtonId >= 4) {
+        setMaskotImage(MaskotTeenSad);
+      } else if (currentButtonId >= 2) {
+        setMaskotImage(MaskotBabySad);
+      } else {
+        setMaskotImage(MaskotEggSad);
+      }
+    }
+  
+    // logika pro nastavení textu maskota
     if (isCorrectAnswer) {
       if (currentButtonId >= 0 && currentButtonId < textyOtazek.length) {
         setMaskotText(textyOtazek[currentButtonId]);
       }
     } else {
-      // Logika pro špatné odpovědi
       setMaskotText(textySpatnychOdpovedi[Math.floor(Math.random() * textySpatnychOdpovedi.length)]);
     }
   }, [currentButtonId, isCorrectAnswer]);
-  
+
   return { 
     isVisible, 
     maskotRef, 
